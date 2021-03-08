81559c2182a82847c131e67c55551fc6d4fdba19
asdfvv <13.y@live.com>
2021-03-08 10:55:22 +0800

    Add files via upload

--- Task/adminCookie.js
+++ Task/adminCookie.js
@@ -1,9 +1,9 @@
 /* ziye  CK导入导出专用    务必下载或者复制到本地使用
 
-github地址 https://github.com/ziye11
+github地址 https://github.com/6Svip120apk69
 TG频道地址  https://t.me/ziyescript
 TG交流群   https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
-boxjs链接  https://raw.githubusercontent.com/ziye11/JavaScript/main/Task/ziye.boxjs.json
+boxjs链接  https://raw.githubusercontent.com/6Svip120apk69/gitee_q8qsTAUA_cThxc1RBVUE/main/Task/ziye.boxjs.json
 
 
 转载请备注个名字，谢谢
